5e161ed36b100e047b6c2b06df869b8334d7e46a
James Mr. Squirrely <jimmiexd@gmail.com>
2020-02-17 18:27:49 -0500

    1.0.2 Update
    Fixed an issue where it was adding the decimalPlaces number to the end of the size.
    Added and Zero and Max option.
    Added a readme.
    Added icon.ico

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Squirrel-Sizer
-A simple to use file sizer
+A simple to use file sizer.
+
+To use it simple.
+
+You give it a long number and it gives you a string with the size abbreviation suffix.
+
+```c#
+long number = 1024;
+Sizer.SizeSuffix(number); // This will output 1.00kb
+```
+
+To use this with files is even as easy.
+
+```c#
+FileInfo info =  new FileInfo("C:\\Path\\To\\File\\file.txt");
+Sizer.SizeSuffix(info.Length); // This will output the converted size of the file.
+// So if the size of the file in bytes is 1024 it will give you 1.00kb
+```
+
+You also have these if you want them
+
+```c#
+Sizer.Zero; // This will give you 0.00bytes
+Sizer.Max // This will give you 8.00EB
+```
+
--- Sizer.cs
+++ Sizer.cs
@@ -1,20 +1,41 @@
-﻿using System;
-
-namespace Squirrel_Sizer {
+﻿namespace Squirrel_Sizer {
     public class Sizer {
+        /// <summary>
+        /// The size suffixes
+        /// </summary>
         private static readonly string[] SizeSuffixes = { "bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
-
+        /// <summary>
+        /// Gets the size suffix from the given parameters
+        /// </summary>
+        /// <param name="value">The size in bytes you wish to convert.</param>
+        /// <param name="decimalPlaces">The decimal places.</param>
+        /// <returns>A string</returns>
+        /// <example>Sizer.SizeSuffix(1024)</example>
         public static string SizeSuffix(long value, int decimalPlaces = 1) {
             if (value < 0) {
                 return $"-{SizeSuffix(-value)}";
             }
             int i = 0;
             decimal dValue = value;
-            while (Math.Round(dValue, decimalPlaces) >= 1000) {
+            while (System.Math.Round(dValue, decimalPlaces) >= 1000) {
                 dValue /= 1024;
                 i++;
             }
-            return $"{dValue:n}{decimalPlaces}{SizeSuffixes[i]}";
+            return $"{dValue:n}{SizeSuffixes[i]}";
         }
+        /// <summary>
+        /// Gets the zero.
+        /// </summary>
+        /// <value>
+        /// The zero.
+        /// </value>
+        public static string Zero => SizeSuffix(0);
+        /// <summary>
+        /// Determines the maximum file size
+        /// </summary>
+        /// <value>
+        /// The maximum.
+        /// </value>
+        public static string Max => SizeSuffix(long.MaxValue);
     }
 }
