4f37e71131618c92300d66731be782ab60ddf594
James Ferguson <jimmiexd@gmail.com>
2020-07-07 15:47:24 -0400

    V1.2
    - Updated readme for new version
    - added size names
    - Renamed SizeSuffix to Suffix and made it private
    - Removed old methods and replaced with new methods
    Todo:
    - Rename Suffix to Size for clarity sakes
    - Remove old obsolete code for cleaness sakes

--- README.md
+++ README.md
@@ -5,44 +5,55 @@ Goto https://www.nuget.org/packages/SquirrelSizer/
 
 To use it is simple.
 
-You give it a long number and it gives you a string with the size abbreviation suffix.
+### You give it a long number and it gives you a string with the size abbreviation suffix.
 
 ```c#
 long number = 1024;
-Sizer.SizeSuffix(number); // This will output 1.00kb
+Sizer.GetSizeSuffix(number); // This will output 1.00kb
 ```
 
-To use this with files is even as easy.
+or 
 
-The new way:
 ```c#
-Sizer.GetSize("C:\\Path\\To\\File\\file.txt"); // This will output the converted size of the file.
+long number = 1024;
+Sizer.GetSizeName(number); // This will output 1.00 Kilobyte
 ```
 
-or
+-------
 
-The old way:
+### To use this with files is even as easy.
+
 ```c#
-FileInfo info =  new FileInfo("C:\\Path\\To\\File\\file.txt");
-Sizer.SizeSuffix(info.Length); // This will output the converted size of the file.
+Sizer.GetSizeSuffix("C:\\Path\\To\\File\\file.txt"); // This will output the converted size of the file.
 ```
 
-You can even get the complete size of multiple files
+or 
 
 ```c#
+Sizer.GetSizeName("C:\\Path\\To\\File\\file.txt"); // This will output the converted size of the file.
+```
 
+-------
+
+### You can even get the complete size of multiple files
+
+```c#
 List<string> files = new List<string>() {
             "C:\\Path\\To\\File\\file1.txt",
             "C:\\Path\\To\\File\\file2.txt",
             "C:\\Path\\To\\File\\file3.txt"
         };
-Sizer.GetCompleteSize(files); // This will output the size of all the files in the list
+Sizer.GetFullSizeSuffix(files); // This will output the size of all the files in the list
 ```
 
-You also have these if you want them
+or
 
 ```c#
-Sizer.Zero; // This will give you 0.00bytes
-Sizer.Max // This will give you 8.00EB
+List<string> files = new List<string>() {
+            "C:\\Path\\To\\File\\file1.txt",
+            "C:\\Path\\To\\File\\file2.txt",
+            "C:\\Path\\To\\File\\file3.txt"
+        };
+Sizer.GetFullSizeName(files); // This will output the size of all the files in the list
 ```
 
--- Sizer.cs
+++ Sizer.cs
@@ -1,48 +1,95 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
 namespace Squirrel_Sizer {
     public static class Sizer {
         private static readonly string[] SizeSuffixes = { "bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
+        private static readonly string[] SizeNames = { "bytes", "Kilobytes", "Megabytes", "Gigabytes", "Terabytes", "Petabytes", "Exabytes", "Zettabytes", "Yottabytes" };
+
+        private static int _index;
+
         /// <summary>
         /// Gets the size suffix from the given parameters
         /// </summary>
         /// <param name="value">The size in bytes you wish to convert.</param>
-        /// <param name="decimalPlaces">The decimal places.</param>
+        /// <param name="decimalPlaces">The number od decimal places you want. Default is 0</param>
         /// <returns>string</returns>
-        /// <example>Sizer.SizeSuffix(1024)</example>
-        public static string SizeSuffix(long value, int decimalPlaces = 0) {
-            if (value < 0) {
-                return $"-{SizeSuffix(-value)}";
-            }
-            int i = 0;
-            decimal dValue = value;
-            while (System.Math.Round(dValue, decimalPlaces) >= 1000) {
-                dValue /= 1024;
-                i++;
-            }
-            return $"{dValue:n}{SizeSuffixes[i]}";
-        }
+        /// <example>Sizer.Suffix(1024)</example>
+        public static string GetSizeSuffix(ulong value, int decimalPlaces = 0) 
+            => $"{decimal.Round(Suffix(value), decimalPlaces)}{SizeSuffixes[_index]}";
         /// <summary>
         /// Returns the size of a given file
         /// </summary>
         /// <param name="file">String location of file</param>
+        /// /// <param name="decimalPlaces">The number od decimal places you want. Default is 0</param>
         /// <returns>string</returns>
-        public static string GetSize(string file) => SizeSuffix(new FileInfo(file).Length);
+        public static string GetSizeSuffix(string file, int decimalPlaces = 0) 
+            => $"{decimal.Round(Suffix((ulong)new FileInfo(file).Length), decimalPlaces)}{SizeSuffixes[_index]}";
         /// <summary>
         /// Returns the complete size of the given files
         /// </summary>
         /// <param name="files">A list of the files</param>
+        /// /// <param name="decimalPlaces">The number od decimal places you want. Default is 0</param>
         /// <returns>string</returns>
-        public static string GetCompleteSize(List<string> files) => SizeSuffix(files.Aggregate(0, (current, file) => (int)(current + new FileInfo(file).Length)));
+        public static string GetFullSizeSuffix(List<string> files, int decimalPlaces = 0) 
+            => $"{decimal.Round(Suffix((ulong)files.Aggregate(0, (current, file) => (int)(current + new FileInfo(file).Length))), decimalPlaces)}{SizeSuffixes[_index]}";
         /// <summary>
-        /// Returns a size of zero
+        /// Gets the size from the given parameters
         /// </summary>
-        public static string Zero => SizeSuffix(0);
+        /// <param name="value">The size in bytes you wish to convert.</param>
+        /// /// <param name="decimalPlaces">The number od decimal places you want. Default is 0</param>
+        /// <returns>string</returns>
+        /// <example>Sizer.Suffix(1024)</example>
+        public static string GetSizeName(ulong value, int decimalPlaces = 0) 
+            => $"{decimal.Round(Suffix(value), decimalPlaces)} {SizeNames[_index]}";
         /// <summary>
-        /// Returns the maximum file size
+        /// Returns the size of a given file
         /// </summary>
-        public static string Max => SizeSuffix(long.MaxValue);
+        /// <param name="file">String location of file</param>
+        /// /// <param name="decimalPlaces">The number od decimal places you want. Default is 0</param>
+        /// <returns>string</returns>
+        public static string GetSizeName(string file, int decimalPlaces = 0) 
+            => $"{decimal.Round(Suffix((ulong)new FileInfo(file).Length), decimalPlaces)} {SizeNames[_index]}";
+        /// <summary>
+        /// Returns the complete size of the given files
+        /// </summary>
+        /// <param name="files">A list of the files</param>
+        /// /// <param name="decimalPlaces">The number od decimal places you want. Default is 0</param>
+        /// <returns>string</returns>
+        public static string GetFullSizeName(List<string> files, int decimalPlaces = 0) 
+            => $"{decimal.Round(Suffix((ulong)files.Aggregate(0, (current, file) => (int)(current + new FileInfo(file).Length))), decimalPlaces)} {SizeNames[_index]}";
+                
+        private static decimal Suffix(ulong value) {
+            _index = 0;
+            decimal dValue = value;
+            while (System.Math.Round(dValue, 1) >= 1000) {
+                dValue /= 1024;
+                _index++;
+            }
+            return dValue;
+        }
+
+
+
+        #region Obsolete Stuff To Remove
+        //Everything in this region will be removed in updated 1.3
+        [Obsolete("This method has been deprecated.", true)]
+        public static string SizeSuffix(long value, int decimalPlaces = 0) => throw new NotImplementedException("This method has been deprecated.");
+
+        [Obsolete("This method has been deprecated. Use GetSizeSuffix or GetSizeName Instead.", true)]
+        public static string GetSize(string file) => throw new NotImplementedException("This method has been deprecated. Use GetSizeSuffix or GetSizeName Instead.");
+
+        [Obsolete("This method has been deprecated. Use GetFullSizeSuffix or GetFullSizeName Instead.", true)]
+        public static string GetCompleteSize(List<string> files) => throw new NotImplementedException("This method has been deprecated. Use GetFullSizeSuffix or GetFullSizeName Instead.");
+
+        [Obsolete("This method has been deprecated.", true)]
+        public static string Zero => throw new NotImplementedException("This method has been deprecated.");
+
+
+        [Obsolete("This method has been deprecated.", true)]
+        public static string Max => throw new NotImplementedException("This method has been deprecated.");
+        #endregion
     }
 }
